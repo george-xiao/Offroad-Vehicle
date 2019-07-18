66fddadd42e40ca1fb0063be5b46f66cb951e5ba
Symoom Saad <saadsymoom@gmail.com>
2019-07-18 10:46:44 -0400

    Formatted the code better, added more comments, got rid of the manual drive and comments on top. Also changed how the program works a tiny bit.

--- ESC.py
+++ ESC.py
@@ -1,12 +1,4 @@
-#Main changes:
-#   i) added try and catches whenever int casting was done
-#  ii) got rid of calibration as our ESC does not (cannot?) perform one
-# iii) changed arm to work with our current ESC
-#Notes:
-#   i) we could get rid of manual drive as it is just a more restrictive control after the changes that you made
-#  ii) imo we can just automatically run arm at first with enter and switch to control after that...really dont need to give user any more option after that 
 # ESC Python program to run the ESC
-# Make sure your battery is not connected if you are going to calibrate it at first.
 
 import os
 import time
@@ -17,46 +9,18 @@ time.sleep(1)
 import pigpio
 
 # Connect the ESC in this GPIO pin
-ESC = 4 #Change to appropriate number later
+ESC = 4 # Change to appropriate number later
 
 pi = pigpio.pi()
-pi.set_servo_pulsewidth(ESC, 0) #Really dont know what this means...the numbers have to be between 1100 and 1900...maybe arm here instead?
+pi.set_servo_pulsewidth(ESC, 0) # Really dont know what this means...test this out and change to 1500 if it doesnt work
 
 # Max and min pulse width values of ESC
 max_value = 1900
 min_value = 1100
-print("Type the exact word for the function you want")
-print("manual OR control OR arm OR stop")
 
-
-# Manually program your ESC if required (Kinda pointless cuz our control already does this)
-def manual_drive():
-    print("You have selected manual option so provide a value between 1100 and 1900")
-    while True:
-        inp = input()
-        if inp == "stop":
-            stop()
-            break
-        elif inp == "control":
-            control()
-            break
-        elif inp == "arm":
-            arm()
-            break
-        else:
-            try:
-                inp = int(inp)
-                if inp < min_value:
-                    inp = min_value
-                elif inp > max_value:
-                    inp = max_value
-                pi.set_servo_pulsewidth(ESC, inp)
-                print("Current speed is: " + inp)
-            except:
-                print("Please enter a valid command")
-
+# Funtion to control the speed of the motor; both incrementally and exactly
 def control(): 
-    # Initial speed, 1500 = 0
+    # Initial speed: 1500 (0 m/s)
     speed = 1500
     print("How To Control The Motor: Initial speed is 1500 (translates to 0 m/s).")
     print("Enter a motor pulse width (1100-1900) or a speed to increase/decrease by.")
@@ -71,9 +35,6 @@ def control():
         if inp == "stop":
             stop()
             break
-        elif inp == "manual":
-            manual_drive()
-            break
         elif inp == "arm":
             arm()
             break
@@ -86,7 +47,7 @@ def control():
                         speed = min_value
                     elif speed > max_value:
                         speed = max_value
-                elif 1100 <= int(inp) <= 1900:
+                elif 1100 <= inp <= 1900:
                     speed = input
                 pi.set_servo_pulsewidth(ESC, speed)
                 print("Current speed is: " + speed)
@@ -94,31 +55,39 @@ def control():
                 print("Please enter a valid command")
 
 
-# This is how to arm our ESC
+# Function to arm Blue Robotics Basic ESC
 def arm():
-    print("Connect the battery and press Enter")
-    inp = input()
-    if inp == '':
-        pi.set_servo_pulsewidth(ESC, 1500)
-        time.sleep(1)
-        control() 
+    while True:
+        print("Connect the battery and press Enter")
+        inp = input()
+        if inp == '':
+            pi.set_servo_pulsewidth(ESC, 1500)
+            time.sleep(1)
+            control()
+            break
+        else:
+            print("Please enter a valid command")
+            
 
-
-# This will stop every action your Pi is performing for the ESC.
+# Function to stop every action the Pi is performing for the ESC.
 def stop():
     pi.set_servo_pulsewidth(ESC, 1500)
     pi.stop()
 
 
-# This is the start of the program.
-inp = input()
-if inp == "manual":
-    manual_drive()
-elif inp == "arm":
-    arm()
-elif inp == "control":
-    control()
-elif inp == "stop":
-    stop()
-else:
-    print("You entered an invalid command, you must now restart the program :(") #Very good use of emojis...PogS
+# Start of the program.
+while True:
+    print("Type the exact word for the function you want")
+    print("control OR arm OR stop")
+
+    inp = input()
+    if inp == "arm":
+        arm()
+    elif inp == "control":
+        control()
+    elif inp == "stop":
+        print("Goodbye!")
+        stop()
+        break
+    else:
+        print("Please enter a valid command") 
