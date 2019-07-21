0eeb172407c0c4de0e2a5d95c32342c38120314b
Symoom Saad <saadsymoom@gmail.com>
2019-07-21 00:38:24 -0400

    Fixed several bugs. Hard coded to prevent motor from spinnning faster than recommended speed. Changed how the motor was controlled (uses the actual y value now). Changed shutdown to power_button. Commented the code more.

--- ESC.py
+++ ESC.py
@@ -2,18 +2,20 @@
 
 import os
 import time
+
 # Launching GPIO library
 os.system("sudo pigpiod")
 # Will cause errors if this delays are removed
 time.sleep(1)
 import pigpio
 
-class ESC:
-# Function to control the speed of the motor; both incrementally and exactly
-    def __init(self, gpio_pin_number)__:
+class ElectronicSpeedController:
+    # Constructor
+    def __init__(self, gpio_pin_number):
         self.ESC = gpio_pin_number
         self.pi = pigpio.pi()
-        self.pi.set_servo_pulsewidth(ESC, 1500)
+        self.pi.set_servo_pulsewidth(self.ESC, 1500)
+
         # Max and min pulse width values of ESC
         self.max_value = 1900
         self.min_value = 1100
@@ -22,15 +24,19 @@ class ESC:
 
     # Function to control the speed of the motor
     def control(self, speed):
-        pi.set_servo_pulsewidth(ESC, speed)
-
+        if speed < self.min_value:
+            speed = self.min_value
+        elif speed > self.max_value:
+            speed = self.max_value
+        self.pi.set_servo_pulsewidth(self.ESC, speed)
+        
 
     # Function to arm Blue Robotics Basic ESC
-    def arm():
-        self.pi.set_servo_pulsewidth(ESC, 1500)
+    def arm(self):
+        self.pi.set_servo_pulsewidth(self.ESC, 1500)
 
          
     # Function to stop every action the Pi is performing for the ESC.
-    def stop():
-        pi.set_servo_pulsewidth(ESC, 1500)
-        pi.stop()
+    def stop(self):
+        self.pi.set_servo_pulsewidth(self.ESC, 1500)
+        self.pi.stop()
--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 from bluedot import BlueDot
-from ESC import ElectronicSpeedController as ESC
+from ESC import ElectronicSpeedController
 from signal import pause
 
 bd = BlueDot()
-robot = ElectronicSpeedController()
+running = True
+robot = ElectronicSpeedController(27)
 
-
+# Function to move and steer the car
+# Uses the distance of the touch from the center of the Dot to determine the car's speed
+# (Future Implementation) Uses the placement of the finger to determine steering direction
+# (Futute Implementation) Uses the angle from 0 degrees to determine how hard to steer
 def move(pos):
-    if pos.top:
-        robot.control(1500 + pos.distance * 400)
-    elif pos.bottom:
-        robot.control(1500 - pos.distance * 400)
-    elif pos.right:
-        pass
-    elif pos.left:
-        pass
+    if running:
+        robot.control(1500 + pos.y * 400)
+        # Need to write code for Servo steering
 
-
+# Function to stop the motor
+# Will stop all motors as soon as there is not input detected from the Blue Dot
 def stop():
     robot.control(1500)
 
+# Function to toggle between power on and power off
+# Double tap to toggle between the power states
+# Power on: Button will be blue
+# Power off: Button will be red
+def power_button():
+    global running
+    running = not(running)
+    if running:
+        bd.color = 'blue'
+    else:
+        bd.color = 'red'
 
-def shutdown():
-    robot.stop()
-
-
-def rotate():
-    robot.rotate()
-
-
+# Allows pairing of other devices for a minute after startup
+bd.allow_pairing(60)
+    
 bd.set_when_pressed(move)
-bd.set_when_double_press(shutdown)
-bd.set_when_rotated(rotate)
 bd.set_when_moved(move)
 bd.set_when_released(stop)
+bd.set_when_double_press(power_button)
 
 pause()
