44fc33491fa5d87040e52a8374298b25ad2da323
George Xiao <g5xiao@edu.uwaterloo.ca>
2019-08-27 23:50:17 -0400

    Added data_logging script
    Creates data.csv and writes sensor data to it

---
+++ data_logging.py
@@ -0,0 +1,44 @@
+from sense_hat import SenseHat
+from datetime import datetime
+from csv
+
+timestamp = datetime.now()
+# Delay in seconds
+delay = 3
+sense = SenseHat()
+
+def get_sense_data():
+	orientation = sense.get_orientation()
+	mag = sense.get_compass_raw()
+	acc = sense.get_accelerometer_raw()
+	gyro = sense.get_gyroscope_raw()
+	return [
+		datetime.now(),
+		sense.get_temperature(),
+		sense.get_pressure(),
+		sense.get_humidity(),
+		orientation['yaw'],
+		orientation['pitch'],
+		orientation['roll'],
+		mag["x"],
+		mag["y"],
+		mag["z"],
+		acc["x"],
+		acc["y"],
+		acc["z"],
+		gyro["x"],
+		gyro["y"],
+		gyro["z"]
+	]
+
+with open('data.csv', 'w', newline='') as f:
+	writer = csv.writer(f)
+	data_writer.writerow(['datetime', 'temperature','pressure','humidity', 'yaw','pitch','roll', 'mag_x','mag_y','mag_z', 'acc_x','acc_y','acc_z', 'gyro_x', 'gyro_y', 'gyro_z'])
+	while True:
+		data = get_sense_data()
+		delta = data[0] - timestamp
+		if delta.seconds > delay:
+			writer.writerow(data)
+			timestamp = datetime.now()
+
+
